431c5db6ea263665a8c53494e4226c74f0c30eac
Patrick Gomes <patrickgomes@gmail.com>
2017-04-22 00:55:31 -0500

    First Commit after completing Ex 3, showing Movies and Customers with Navigation referenes

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,9 +1,22 @@
-﻿
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required][StringLength(100)]
         public string Name { get; set; }
-    }
+        [Required]
+        public byte GenreId { get; set; }
+        
+        public Genre Genre { get; set; }
+        
+        public DateTime ReleaseDate { get; set; }
+        
+        public DateTime DateAdded { get; set; }
+        
+        public byte NumberInStock { get; set; }
+     }
 }
